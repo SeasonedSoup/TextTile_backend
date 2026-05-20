38ca2ce77094629b9d3e85520205cc5583ff4dff
SeasonedSoup <emmetteagoncillo1@gmail.com>
2026-05-20 11:39:32 +0800

    Add passport functionality in index.js

--- index.js
+++ index.js
@@ -4,7 +4,37 @@ const cors = require('cors');
 
 //PASSPORT
 const passport = require('passport');
-const localStrategy = require('passport-local').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 const bcryptjs = require('bcryptjs');
 
+require('dotenv').config();
+
+const PORT = process.env.PORT;
 const app = express();
+
+//fetch user check if exists check if password is correct
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const user = await prisma.user.findUnique({
+                where: {
+                    username: username
+                }
+            })
+
+            if (!user) {
+                return done(null, false, {message: "Username does not exist"});
+            }
+
+            const match = await bcryptjs.compare(password, user.password);
+
+            if (!match) {
+                return done(null, false, {message: "Incorrect password"});
+            }
+
+            return done(null, user);
+
+        } catch (err) {
+            return done(err);
+        }
+    }));
